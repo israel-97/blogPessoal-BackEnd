fcce8976cacde3a80d46bceeff2f76782704e592
israel-97 <israel.dantas1@gmail.com>
2021-06-29 18:58:09 -0300

    consumindo api

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -1,6 +1,8 @@
 package org.generation.blogPessoal.model;
 
 public class UserLogin {
+	
+	private long id;
 
 	private String nome;
 	
@@ -9,7 +11,35 @@ public class UserLogin {
 	private String senha;
 	
 	private String token;
+	
+	private String foto;
+	
+	private String tipo;
+	
+	public String getFoto() {
+		return foto;
+	}
 
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
